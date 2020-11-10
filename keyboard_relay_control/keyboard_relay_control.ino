7c94fe086b1fcb844f3bc1b015e54d475eab75cb
Kokorev.Yaroslav <kokorev.yaroslav@gmail.com>
2020-11-10 22:33:08 +0300

    add gitignore. add init of keypad

--- keyboard_relay_control/keyboard_relay_control.ino
+++ keyboard_relay_control/keyboard_relay_control.ino
@@ -1,6 +1,21 @@
 #include <Keypad.h>
 #include <Key.h>
 
+const byte ROWS = 4;
+const byte COLS = 4;
+
+const char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+const byte row_pins[ROWS] = {14, 15, 16, 17};
+const byte col_pins[COLS] = {18, 19, 20, 21};
+
+Keypad keypad = Keypad(makeKeymap(keys), row_pins, col_pins, ROWS, COLS);
+
 void setup() {
 
 }

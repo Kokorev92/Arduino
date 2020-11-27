db739a52b16701ddf5011516578d9a2054b79348
Kokorev.Yaroslav <kokorev.yaroslav@gmail.com>
2020-11-28 00:14:28 +0300

    feature: вращение в одну сторону

--- l298n_tester/l298n_tester.ino
+++ l298n_tester/l298n_tester.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Stepper.h>
 
+Stepper motor(18,2,3,4,5);
+
+void setup() {
+  for(int i = 2; i < 6; i++){
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  motor.step(1);
+  delay(10);
 }

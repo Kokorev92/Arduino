5a2bb60ca23d4a56767caa1b256e0269be3d74d0
Kokorev.Yaroslav <kokorev.yaroslav@gmail.com>
2020-12-24 23:58:57 +0300

    fix: отмена мелкой правки, удаление старого кода

--- thermistor/thermistor.ino
+++ thermistor/thermistor.ino
@@ -27,7 +27,7 @@ Average average;
 const byte tempPin = A0;
 const int B =  3950; // B-коэффициент
 const int SERIAL_R = 100000; // сопротивление последовательного резистора, 102 кОм
-const int MTHERMISTOR_R = 100000; // номинальное сопротивления термистора, 100 кОм
+const int THERMISTOR_R = 100000; // номинальное сопротивления термистора, 100 кОм
 const int NOMINAL_T = 25; // номинальная температура (при которой TR = 100 кОм)
 
 void setup() {
@@ -36,22 +36,16 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 200; i++) {
+  for (int i = 0; i < 20; i++) {
     int t = analogRead( tempPin );
     float tr = 1023.0 / t - 1;
     tr = SERIAL_R / tr;
 
-    float steinhart;
-    steinhart = tr / THERMISTOR_R; // (R/Ro)
-    steinhart = log(steinhart); // ln(R/Ro)
-    steinhart /= B; // 1/B * ln(R/Ro)
-    steinhart += 1.0 / (NOMINAL_T + 273.15); // + (1/To)
-    steinhart = 1.0 / steinhart; // Invert
-    steinhart -= 273.15;
-    average.add_value(steinhart);
+    float result = (1.0 / ((1.0 / (NOMINAL_T + 273.15)) + (1.0 / B) * log(tr / THERMISTOR_R))) - 273.15;
+    average.add_value(result);
     delay(5);
   }
-  Serial.print("T=");
+//  Serial.print("T=");
   Serial.println(average.get_value());
   average.reset();
 }

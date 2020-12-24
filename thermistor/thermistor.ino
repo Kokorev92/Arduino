c8c426cecb67272d0be9fbc2e7af74bebbc9ad2e
Kokorev.Yaroslav <kokorev.yaroslav@gmail.com>
2020-12-24 22:08:32 +0300

    feature: получение  данных с термистора с усреднением значений

---
+++ thermistor/thermistor.ino
@@ -0,0 +1,61 @@
+#define B 3950 // B-коэффициент
+#define SERIAL_R 100000 // сопротивление последовательного резистора, 102 кОм
+#define THERMISTOR_R 100000 // номинальное сопротивления термистора, 100 кОм
+#define NOMINAL_T 25 // номинальная температура (при которой TR = 100 кОм)
+
+class Average {
+
+  public:
+    float add_value(float val) {
+      stat_value_ = (stat_value_ * (float)(count_) + val) / ((float)(count_) + 1.0);
+      count_++;
+      return stat_value_;
+    }
+
+    float get_value() {
+      return stat_value_;
+    }
+
+    void reset() {
+      count_ = 0;
+      stat_value_ = 0;
+    }
+
+  private:
+    int count_ = 0;
+    float stat_value_ = 0;
+
+};
+
+Average average;
+
+const byte tempPin = A0;
+
+void setup() {
+  Serial.begin( 9600 );
+  pinMode( tempPin, INPUT );
+}
+
+void loop() {
+  for (int i = 0; i < 200; i++) {
+    int t = analogRead( tempPin );
+    float tr = 1023.0 / t - 1;
+    tr = SERIAL_R / tr;
+//    Serial.print("R=");
+//    Serial.print(tr);
+//    Serial.print(", t=");
+
+    float steinhart;
+    steinhart = tr / THERMISTOR_R; // (R/Ro)
+    steinhart = log(steinhart); // ln(R/Ro)
+    steinhart /= B; // 1/B * ln(R/Ro)
+    steinhart += 1.0 / (NOMINAL_T + 273.15); // + (1/To)
+    steinhart = 1.0 / steinhart; // Invert
+    steinhart -= 273.15;
+    average.add_value(steinhart);
+    delay(5);
+  }
+  Serial.print("T=");
+  Serial.println(average.get_value());
+  average.reset();
+}

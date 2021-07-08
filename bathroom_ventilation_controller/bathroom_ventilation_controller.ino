5032147cadb9beb1ef560f1f65c9195908c5bdb1
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-07-09 00:41:11 +0300

    [wip]: убрал пин 13, сделал все номера пинов через переменые

--- bathroom_ventilation_controller/bathroom_ventilation_controller.ino
+++ bathroom_ventilation_controller/bathroom_ventilation_controller.ino
@@ -1,4 +1,10 @@
-static const int DELAY_TIME_MS = 5000;
+static const float TIME_M = 10;
+static const int DELAY_TIME_MS = 1000 * 60 * TIME_M;
+
+static const int START_BUTTON = 2;
+static const int STOP_BUTTON = 3;
+static const int RELAY = 4;
+
 bool work = false;
 uint32_t start_work_time;
 
@@ -6,34 +12,30 @@ auto ON = !HIGH;
 auto OFF = !LOW;
 
 void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(4, OUTPUT);
-  digitalWrite(4, OFF);
-  digitalWrite(13, LOW);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, OFF);
+  pinMode(START_BUTTON, INPUT_PULLUP);
+  pinMode(STOP_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
-  if (!digitalRead(2)) {
+  if (!digitalRead(START_BUTTON)) {
     work = true;
     start_work_time = millis();
   }
 
-  if(!digitalRead(3)){
+  if (!digitalRead(STOP_BUTTON)) {
     work = false;
   }
 
   if (work) {
     auto current_time = millis();
     if ((current_time - start_work_time) <= DELAY_TIME_MS) {
-      digitalWrite(13, HIGH);
-      digitalWrite(4, ON);
+      digitalWrite(RELAY, ON);
     } else {
       work = false;
     }
   } else {
-    digitalWrite(13, LOW);
-    digitalWrite(4, OFF);
+    digitalWrite(RELAY, OFF);
   }
 }

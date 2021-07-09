594cabf9d95f93266ca2ae383dd28a1077ab6946
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-07-09 13:26:16 +0300

    [feature]: изменил переменную времени, добавил отладочный вывод. Добавил небольшой делей

--- bathroom_ventilation_controller/bathroom_ventilation_controller.ino
+++ bathroom_ventilation_controller/bathroom_ventilation_controller.ino
@@ -1,5 +1,6 @@
-static const float TIME_M = 10;
-static const int DELAY_TIME_MS = 1000 * 60 * TIME_M;
+#include "debug_print.h"
+
+static const float TIME_M = 1;
 
 static const int START_BUTTON = 2;
 static const int STOP_BUTTON = 3;
@@ -7,6 +8,7 @@ static const int RELAY = 4;
 
 bool work = false;
 uint32_t start_work_time;
+uint32_t delay_time_ms = 0;
 
 auto ON = !HIGH;
 auto OFF = !LOW;
@@ -16,6 +18,7 @@ void setup() {
   digitalWrite(RELAY, OFF);
   pinMode(START_BUTTON, INPUT_PULLUP);
   pinMode(STOP_BUTTON, INPUT_PULLUP);
+  delay_time_ms = TIME_M * 60 * 1000;
 }
 
 void loop() {
@@ -30,7 +33,7 @@ void loop() {
 
   if (work) {
     auto current_time = millis();
-    if ((current_time - start_work_time) <= DELAY_TIME_MS) {
+    if ((current_time - start_work_time) <= delay_time_ms) {
       digitalWrite(RELAY, ON);
     } else {
       work = false;
@@ -38,4 +41,6 @@ void loop() {
   } else {
     digitalWrite(RELAY, OFF);
   }
+
+  delay(10);
 }

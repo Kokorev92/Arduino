fbbaec23298a4801260adc97aae84da8c1d63a2f
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-10-12 17:10:14 +0300

    [feature]: реализовал простой протокол с парсером для адруино

---
+++ serial_protocol/parser.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+enum class STATE { IDLE = 0, GOT_HEADER, GOT_CMD, GOT_FOOTER, GOT_PACKET };
+
+class Parser {
+public:
+    void add_byte(uint8_t byte);
+
+    bool get_command(uint8_t& cmd);
+
+private:
+    uint8_t crc_          = 0;
+    uint8_t cmd_          = 0;
+    STATE   parser_state_ = STATE::IDLE;
+
+    void calc_crc(uint8_t byte) {
+        crc_ += byte;
+    }
+};
--- serial_protocol/serial_protocol.ino
+++ serial_protocol/serial_protocol.ino
@@ -1,9 +1,39 @@
-#include "protocol.h"
+#include "parser.h"
+
+const uint8_t relay_1_pin = 6;
+const uint8_t relay_2_pin = 7;
+
+const uint8_t pins[] = {relay_1_pin, relay_2_pin};
+
+struct packet_t {
+  bool relay_1 : 1;
+  bool relay_2 : 1;
+};
+
+packet_t packet;
+Parser parser;
+uint8_t command = 0;
 
 void setup() {
+  for (auto pin : pins) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
 
+  Serial.begin(19200);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    byte b = Serial.read();
 
+    parser.add_byte(b);
+  }
+
+  if(parser.get_command(command)){
+    memcpy(&packet, &command, sizeof(command));
+  }
+
+  digitalWrite(relay_1_pin, packet.relay_1);
+  digitalWrite(relay_2_pin, packet.relay_2);
 }

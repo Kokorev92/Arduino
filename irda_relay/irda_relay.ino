515fc54d21fe0d0da1518aade8bb3b47512f1477
Kokorev92 <kokorev.yaroslav@gmail.com>
2021-10-26 22:44:17 +0300

    [fix]: relay control

--- irda_relay/irda_relay.ino
+++ irda_relay/irda_relay.ino
@@ -1,22 +1,21 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
 
-const int IR_PIN = 7;
-const int RELAY_PIN = 10;
+const int IR_PIN = 2;
+const int RELAY_PIN = 3;
 
 IRrecv resv(IR_PIN);
 decode_results result;
 
 void setup() {
-  Serial.begin(9600);
   resv.enableIRIn();
   pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
 }
 int on = 0;
 void loop() {
-  if(resv.decode(&result)) {
-    Serial.println(result.value, HEX);
-    if(result.value == 0x20DF10EF) {
+  if (resv.decode(&result)) {
+    if (result.value == 0xFF38C7) {
       on = !on;
       digitalWrite(RELAY_PIN,  on ? HIGH : LOW);
     }

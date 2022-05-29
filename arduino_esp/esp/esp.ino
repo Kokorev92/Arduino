2578f684b042c2c6d780edf4774ca946607659f9
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-05-29 13:05:47 +0300

    [wip]: управление реле через ардуинку

--- arduino_esp/esp/esp.ino
+++ arduino_esp/esp/esp.ino
@@ -1,9 +1,14 @@
 #include <ESP8266WiFi.h>
+#include "index.h"
 
 const char* ssid = "esp_ap";
 const char* password = "12345678";
 
 WiFiServer server(80);
+
+
+volatile uint8_t packet[4] = {0x3a, 0x0, 0x21, 0x0};
+volatile uint8_t cmd = 0;
 
 void setup() {
   Serial.begin(19200);
@@ -19,7 +24,17 @@ void setup() {
   server.begin();
 }
 
+
 void loop() {
+  packet[1] = cmd;
+  for (int i = 0; i < 3; i++) {
+    packet[3] += packet[i];
+  }
+
+  //  Serial.write(packet, 4);
+  Serial.println((int)cmd);
+  delay(100);
+
   WiFiClient client = server.available();
   if (!client) {
     return;
@@ -27,9 +42,20 @@ void loop() {
 
   client.setNoDelay(true);
 
-  while (client.connected()) {
-    while (client.available()) {
-      Serial.write(client.read());
-    }
+  String req = client.readStringUntil('\r');
+  client.flush();
+  
+  if (req.indexOf("/REL/1") != -1) {
+    cmd ^= (1 << 0);
+    return;
+  } else if (req.indexOf("/REL/2") != -1) {
+    cmd ^= (1 << 1);
+    return;
+  } else if (req.indexOf("/ HTTP") != -1) {
+    client.print(html);
+    client.flush();
+    return;
+  } else {
+    client.stop();
   }
 }
